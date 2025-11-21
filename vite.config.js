d6101b891c6f218521a91936eab99bdc7517dcd2
Arghelus <arghelus31@gmail.com>
2025-11-21 15:50:21 -0500

    Ajuste configuración Vite y Render: base '/' y rewrites para SPA

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Asegura que todas las rutas de tu SPA funcionen
   server: {
-    host:'localhost',
-    port: 9050,}
+    host: true, // true permite acceder desde cualquier dispositivo de la red
+    port: 9050,
+  },
 })
