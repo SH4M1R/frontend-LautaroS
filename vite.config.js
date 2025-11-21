53b5c7b833bfd133ce3aa21c1616fdbc5e378e07
Arghelus <arghelus31@gmail.com>
2025-11-21 16:04:05 -0500

    Ajuste SPA: refresco de rutas y deploy seguro en Render

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Asegura que todas las rutas de tu SPA funcionen
-  server: {
-    host: true, // true permite acceder desde cualquier dispositivo de la red
-    port: 9050,
-  },
+  // base: "/", // opcional, ya es "/" por defecto
 })
